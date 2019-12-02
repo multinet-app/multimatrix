c8472c18c5cedbe28eae70d2a5872cd85138ae77
JackWilb <jackwilburn@tutanota.com>
2019-12-02 11:34:34 -0700

    Fix the first of the 2 tests, test highlighting on click

--- src/__tests__/e-2-e.test.js
+++ src/__tests__/e-2-e.test.js
@@ -32,10 +32,16 @@ describe("End to end", () => {
             node = await page.evaluate(() => document.querySelector("#topoRownodes-1").classList)
             expect(node[1]).toBe(undefined);
 
+            node = await page.evaluate(() => document.querySelector("#topoColnodes-1").classList)
+            expect(node[1]).toBe(undefined);
+
             // Should be clicked on click
             await page.click("#rowLabelnodes-1");
             await page.mouse.move(0, 0);
             node = await page.evaluate(() => document.querySelector("#topoRownodes-1").classList)
+            expect(node[1]).toBe("clicked");
+
+            node = await page.evaluate(() => document.querySelector("#topoColnodes-1").classList)
             expect(node[1]).toBe("clicked");
 
             // Should not be clicked after clicking again
@@ -44,19 +50,8 @@ describe("End to end", () => {
             node = await page.evaluate(() => document.querySelector("#topoRownodes-1").classList)
             expect(node[1]).toBe(undefined);
 
-        });
-
-        it("Dragging node moves it", async() => {
-            await page.goto("http://127.0.0.1:8082/?workspace=test&graph=test");
-            await page.waitForSelector(".nodeGroup");
-            await page.click(".nodeGroup");
-            e = await page.$(".nodeGroup");
-            box = await e.boundingBox();
-            await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
-            await page.mouse.down();
-            await page.mouse.move(1300, 500);
-            await page.mouse.up();
-            await page.click(".nodeGroup");
+            node = await page.evaluate(() => document.querySelector("#topoColnodes-1").classList)
+            expect(node[1]).toBe(undefined);
 
         });
     });
