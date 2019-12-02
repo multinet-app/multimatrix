e636683b87a63465b432234c8fae0989d5a0922c
JackWilb <jackwilburn@tutanota.com>
2019-12-02 11:33:23 -0700

    Fix the first of the 2 tests, test highlighting on click

--- src/__tests__/e-2-e.test.js
+++ src/__tests__/e-2-e.test.js
@@ -6,8 +6,9 @@ const height = 1080;
 
 beforeAll(async() => {
     browser = await puppeteer.launch({
-        headless: true,
-        args: [`--window-size=${width},${height}`]
+        headless: false,
+        args: [`--window-size=${width},${height}`],
+        slowMo: 100
     });
     page = await browser.newPage();
     await page.setViewport({ width, height });
@@ -19,27 +20,29 @@ afterAll(() => {
 
 describe("End to end", () => {
     describe("Visualization", () => {
-        it("Clicking on node highlights it", async() => {
+        it("Clicking on node highlights row and column", async() => {
 
             // Arrange
             let node, value;
             await page.goto("http://127.0.0.1:8082/?workspace=test&graph=test");
-            await page.waitForSelector(".nodeGroup");
+            await page.waitForSelector(".cell");
 
             // Act + Assert
             // Should not be clicked when started
-            node = await page.evaluate(() => document.querySelectorAll(".node.nodeBox")[0].classList)
-            expect(node[2]).toBe(undefined);
+            node = await page.evaluate(() => document.querySelector("#topoRownodes-1").classList)
+            expect(node[1]).toBe(undefined);
 
             // Should be clicked on click
-            await page.click(".node.nodeBox");
-            node = await page.evaluate(() => document.querySelectorAll(".node.nodeBox")[0].classList)
-            expect(node[2]).toBe("clicked");
+            await page.click("#rowLabelnodes-1");
+            await page.mouse.move(0, 0);
+            node = await page.evaluate(() => document.querySelector("#topoRownodes-1").classList)
+            expect(node[1]).toBe("clicked");
 
             // Should not be clicked after clicking again
-            await page.click(".node.nodeBox");
-            node = await page.evaluate(() => document.querySelectorAll(".node.nodeBox")[0].classList)
-            expect(node[2]).toBe(undefined);
+            await page.click("#rowLabelnodes-1");
+            await page.mouse.move(0, 0);
+            node = await page.evaluate(() => document.querySelector("#topoRownodes-1").classList)
+            expect(node[1]).toBe(undefined);
 
         });
 
