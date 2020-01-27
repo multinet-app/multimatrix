b1c0554bf0700885b950e6e98f9219daee51878a
JackWilb <jackwilburn@tutanota.com>
2020-01-27 14:40:15 -0700

    remove some provenance code that wasn't being used

--- src/lib/provenance.ts
+++ src/lib/provenance.ts
@@ -4,23 +4,6 @@
  */
 import * as Provenance from 'provenance-lib-core/lib/src/provenance-core/Provenance';
 
-function _nodeLink(provenance: Provenance.Provenance<{
-  order: string; nodePos: {}; // map of node positions,
-    userSelectedNeighbors: never[]; // map of nodes that have neighbors selected (so they can be non-muted)
-    userSelectedEdges: never[];
-    selected: never[]; // set of nodes that have been 'soft selected'
-    hardSelected: never[]; // set of nodes that have been 'hard selected'
-    search: never[]; // field to store the id of a searched node;
-    startTime: Date; // time this provenance graph was created and the task initialized;
-    event: string; // string describing what event triggered this state update;
-    //  endTime:'', // time the submit button was pressed and the task ended;
-    time: Date;
-  }>) {
-  return {
-    currentState: () => provenance.graph().current.state,
-  };
-}
-
 // Initialize the state object for node positions;
 function _nodePositionMap(nodes: any[]) {
   const nodeMap: {[id: string]: any} = {};
@@ -28,7 +11,7 @@ function _nodePositionMap(nodes: any[]) {
   return nodeMap;
 }
 
-function setUpProvenance(nodes: any, order = 'noOrder') {
+export function setUpProvenance(nodes: any, order = 'noOrder') {
   const nodePos = _nodePositionMap(nodes);
 
   const initialState = {
@@ -41,18 +24,13 @@ function setUpProvenance(nodes: any, order = 'noOrder') {
     search: [], // field to store the id of a searched node;
     startTime: new Date(), // time this provenance graph was created and the task initialized;
     event: 'startedPvenance', // string describing what event triggered this state update;
-    //  endTime:'', // time the submit button was pressed and the task ended;
     time: new Date(), // timestamp for the current state of the graph;
   };
 
   const provenance = Provenance.initProvenance(initialState);
-  const app = _nodeLink(provenance);
+  const app = provenance.graph().current.state;
   return {
     provenance,
     app,
   };
 }
-
-export {
-  setUpProvenance,
-};
