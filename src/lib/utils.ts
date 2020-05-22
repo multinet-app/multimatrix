4a0a6ce0ea002342e94e5e7bf758a24aad0881fa
JackWilb97 <jackwilburn@tutanota.com>
2020-05-22 14:25:32 -0600

    use graph in query string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,14 @@
 // Get the url querystring variables
 export function getUrlVars(): { [key: string]: string} {
   const workspace: string = '';
-  const network: string = '';
-  const vars: { [key: string]: string} = {workspace, network};
+  const graph: string = '';
+  const vars: { [key: string]: string} = {workspace, graph};
   window.location.href.replace(/[?&]+([^=&]+)=([^&*#]*)/gi,
     (m: any, key: string, value: any) => {
       vars[key] = value;
       return value;
     },
   );
-  vars.networkName = vars.network;
+  vars.networkName = vars.graph;
   return vars;
 }
