da7b5c73b91174a73528210a174e19f5b1abd682
Pranav Rajan <rajan.pranav320@gmail.com>
2020-08-23 22:46:51 -0700

    commented out all the row labels

--- src/components/AdjMatrix/AdjMatrixMethods.ts
+++ src/components/AdjMatrix/AdjMatrixMethods.ts
@@ -30,7 +30,7 @@ export class View {
   private edgeRows: any;
   private edgeColumns: any;
   private edgeScales!: { [key: string]: any };
-  private nodeFontSize: string = '12';
+  private nodeFontSize: string = '10';
   private columnHeaders: any;
   private attributeScales: { [key: string]: any } = {};
   private colMargin: number = 5;
@@ -242,10 +242,10 @@ export class View {
     const sideLength = Math.min(this.matrixWidth, this.matrixHeight);
 
     // set the dimensions of a cell
-    const cellSize = 11;
+    const cellSize = 15;
 
     // set the radius for cells
-    const cellRadius = 2;
+    const cellRadius = 3;
 
     // set the size of the number of nodes
     const matrixNodeLength = this.network.nodes.length;
@@ -472,30 +472,30 @@ export class View {
    * @return none
    */
   private appendEdgeLabels(): void {
-    this.edgeRows.append('text')
-      .attr('class', 'rowLabel')
-      .attr('id', (d: Node) => `rowLabel${d.id}`)
-      .attr('z-index', 30)
-      .attr('x', -3)
-      .attr('y', this.orderingScale.bandwidth() / 2)
-      .attr('dy', '.32em')
-      .attr('text-anchor', 'end')
-      .style('font-size', this.nodeFontSize.toString() + 'pt')
-      .text((d: Node) => d._key)
-      .on('mouseover', (d: Node) => this.hoverNode(d.id))
-      .on('mouseout', (d: Node) => this.unHoverNode(d.id))
-      .on('mouseover', (d: Node, i: number, nodes: any) => {
-        this.showToolTip(d, i, nodes);
-        this.hoverNode(d.id);
-      })
-      .on('mouseout', (d: Node) => {
-        this.hideToolTip();
-        this.unHoverNode(d.id);
-      })
-      .on('click', (d: Node) => {
-        this.selectElement(d);
-        this.selectNeighborNodes(d.id, d.neighbors);
-      });
+    // this.edgeRows.append('text')
+    //   .attr('class', 'rowLabel')
+    //   .attr('id', (d: Node) => `rowLabel${d.id}`)
+    //   .attr('z-index', 30)
+    //   .attr('x', -3)
+    //   .attr('y', this.orderingScale.bandwidth() / 2)
+    //   .attr('dy', '.32em')
+    //   .attr('text-anchor', 'end')
+    //   .style('font-size', this.nodeFontSize.toString() + 'pt')
+    //   .text((d: Node) => d._key)
+    //   .on('mouseover', (d: Node) => this.hoverNode(d.id))
+    //   .on('mouseout', (d: Node) => this.unHoverNode(d.id))
+    //   .on('mouseover', (d: Node, i: number, nodes: any) => {
+    //     this.showToolTip(d, i, nodes);
+    //     this.hoverNode(d.id);
+    //   })
+    //   .on('mouseout', (d: Node) => {
+    //     this.hideToolTip();
+    //     this.unHoverNode(d.id);
+    //   })
+    //   .on('click', (d: Node) => {
+    //     this.selectElement(d);
+    //     this.selectNeighborNodes(d.id, d.neighbors);
+    //   });
 
     let verticalOffset = 187.5;
     const horizontalOffset = (this.orderingScale.bandwidth() / 2 - 4.5) / 0.075;
