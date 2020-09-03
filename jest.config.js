9eca86319f3b653229664acbcdcf59b4629d7312
JackWilb97 <jackwilburn@tutanota.com>
2020-09-03 13:14:13 -0700

    run prettier on the codebase

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    transformIgnorePatterns: ['/node_modules/(?!vuetify)'],
-    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  transformIgnorePatterns: ['/node_modules/(?!vuetify)'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
 
-    transform: {
+  transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-        'jest-transform-stub',
-    '^.+\\.(js|jsx)?$': 'babel-jest'
-    },
+      'jest-transform-stub',
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
 
-    preset: '@vue/cli-plugin-unit-jest'
-}
+  preset: '@vue/cli-plugin-unit-jest',
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,10 @@
 // Get the url querystring variables
-export function getUrlVars(): { [key: string]: string} {
+export function getUrlVars(): { [key: string]: string } {
   const workspace: string = '';
   const graph: string = '';
-  const vars: { [key: string]: string} = {workspace, graph};
-  window.location.href.replace(/[?&]+([^=&]+)=([^&*#]*)/gi,
+  const vars: { [key: string]: string } = { workspace, graph };
+  window.location.href.replace(
+    /[?&]+([^=&]+)=([^&*#]*)/gi,
     (m: any, key: string, value: any) => {
       vars[key] = value;
       return value;
