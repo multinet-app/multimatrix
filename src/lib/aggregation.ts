815050801872c17a834e795bc9bf77b603dff01b
Pranav Rajan <rajan.pranav320@gmail.com>
2021-01-12 11:58:24 -0800

    Remove link.source

--- src/lib/aggregation.ts
+++ src/lib/aggregation.ts
@@ -69,7 +69,6 @@ export function superGraph(nodes: Node[], edges: Link[], attribute: string) {
         if (linkFrom === origin) {
           const newLinkFrom = superNode.id;
           link._from = newLinkFrom;
-          link.source = link._from;
         }
         if (linkTo === origin) {
           const newLinkTo = superNode.id;
