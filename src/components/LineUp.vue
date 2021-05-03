91d0423504cff83290795a9045b48f2645760b4e
JackWilb <jackwilburn@tutanota.com>
2021-05-03 13:27:47 -0600

    Simplify some logic, add lineup -> matrix sorting functionality

--- src/components/LineUp.vue
+++ src/components/LineUp.vue
@@ -15,6 +15,24 @@ export default {
     const lineup: Ref<LineUp | null> = ref(null);
     const builder: Ref<DataBuilder | null> = ref(null);
 
+    // Helper functions
+    function idsToIndices(ids: string[]) {
+      return ids
+        .map((elementID) => (builder.value !== null
+          ? builder.value.buildData().data.findIndex((dataElement) => dataElement._id === elementID)
+          : -1))
+        .filter((index) => index !== -1);
+    }
+
+    function indicesToIDs(indicies: number[]) {
+      return indicies.map((index: number) => {
+        if (builder.value !== null) {
+          return builder.value.buildData().data[index]._id.toString();
+        }
+        return undefined;
+      });
+    }
+
     // We have to use nextTick so that the component is rendered
     nextTick(() => {
       const lineupDiv = document.getElementById('lineup');
@@ -33,17 +51,12 @@ export default {
         // Add an event watcher to update selected nodes
         lineup.value.on('selectionChanged', (dataindices: number[]) => {
           // Transform data indices to multinet `_id`s
-          const clickedIDs: string[] = dataindices.map((index: number) => {
-            if (builder.value !== null) {
-              return builder.value.buildData().data[index]._id.toString();
-            }
-            return undefined;
-          });
+          const clickedIDs: string[] = indicesToIDs(dataindices);
 
           // Find the symmetric difference between the ids here and those in the store
           function diffFunction<T>(arr1: Array<T>, arr2: Array<T>): Array<T> { return arr1.filter((x) => arr2.indexOf(x) === -1); }
-          const differentIDs = diffFunction<string>(clickedIDs, [...selectedNodes.value])
-            .concat(diffFunction([...selectedNodes.value], clickedIDs));
+          const differentIDs = diffFunction<string>(clickedIDs, selectedNodes.value)
+            .concat(diffFunction(selectedNodes.value, clickedIDs));
 
           // Click on the elements that are different to add/remove them from the store
           differentIDs.forEach((nodeID) => store.commit.clickElement(nodeID));
@@ -51,23 +64,33 @@ export default {
       }
     });
 
+    // Update selection from matrix
     watchEffect(() => {
       // Convert the ids to indices
-      const indices = selectedNodes.value
-        .map((elementID) => (builder.value !== null
-          ? builder.value.buildData().data.findIndex((dataElement) => dataElement._id === elementID)
-          : -1))
-        .filter((index) => index !== -1);
+      const indices = idsToIndices(selectedNodes.value);
 
       if (lineup.value !== null) {
         lineup.value.setSelection(indices);
       }
     });
 
+    let currentLineupSortOrder: number[] = [];
+
+    // Update sort order in matrix
     watchEffect(() => {
       if (lineup.value !== null) {
-        console.log(lineup.value);
-        console.log(lineup.value.data.rankings[0].order);
+        const lineupOrder = [...lineup.value.data.getFirstRanking().getOrder()];
+        const storeOrder = store.state.sortOrder;
+
+        console.log(lineupOrder, lineup.value.data.getFirstRanking());
+
+        if (JSON.stringify(currentLineupSortOrder) !== JSON.stringify(lineupOrder)) {
+          // If lineup order has changed, update the store
+          store.commit.setSortOrder(lineupOrder);
+          currentLineupSortOrder = lineupOrder;
+        } else if (JSON.stringify(currentLineupSortOrder) !== JSON.stringify(storeOrder)) {
+          // If store order has changed, update lineup
+        }
       }
     });
 
