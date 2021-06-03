31d7e12ae59cc8a5d844f823b9ebcfd564867b20
JackWilb <jackwilburn@tutanota.com>
2021-06-03 12:14:11 -0600

    Add provenance utils and store related functions

---
+++ src/lib/provenanceUtils.ts
@@ -0,0 +1,42 @@
+import { ProvenanceEventTypes, State } from '@/types';
+import { createAction } from '@visdesignlab/trrack';
+
+export function updateProvenanceState(vuexState: State, label: ProvenanceEventTypes) {
+  /* eslint-disable no-param-reassign */
+
+  const stateUpdateActions = createAction<State, State[], ProvenanceEventTypes>((provState, newProvState) => {
+    if (label === 'Set Select Neighbors') {
+      provState.selectNeighbors = newProvState.selectNeighbors;
+    }
+
+    /* eslint-enable no-param-reassign */
+  })
+    .setLabel(label);
+
+  if (vuexState.provenance !== null) {
+    vuexState.provenance.apply(stateUpdateActions(vuexState));
+  }
+}
+
+export function undoRedoKeyHandler(event: KeyboardEvent, storeState: State) {
+  // If provenance doesn't exist, exit
+  if (storeState.provenance == null) { return; }
+
+  if (
+    (event.ctrlKey && event.code === 'KeyZ' && !event.shiftKey) // ctrl + z (no shift)
+      || (event.metaKey && event.code === 'KeyZ' && !event.shiftKey) // meta + z (no shift)
+  ) {
+    if (storeState.provenance.current.id !== storeState.provenance.root.id) {
+      storeState.provenance.undo();
+    }
+  } else if (
+    (event.ctrlKey && event.code === 'KeyY') // ctrl + y
+      || (event.ctrlKey && event.code === 'KeyZ' && event.shiftKey) // ctrl + shift + z
+      || (event.metaKey && event.code === 'KeyY') // meta + y
+      || (event.metaKey && event.code === 'KeyZ' && event.shiftKey) // meta + shift + z
+  ) {
+    if (storeState.provenance.current.children.length > 0) {
+      storeState.provenance.redo();
+    }
+  }
+}
