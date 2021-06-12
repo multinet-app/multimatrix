3dabb50ea962d60ea4ca642bc7ae7f7f85cc0f17
derya <deryakbaba22@gmail.com>
2021-06-11 23:44:23 -0600

    Switch to composition API

--- src/components/ConnectivityQuery.vue
+++ src/components/ConnectivityQuery.vue
@@ -17,37 +17,41 @@
           v-for="i in displayedHops"
           :key="i"
         >
-          <v-list-item-content>
-            <v-list-item-title
-              v-if="i % 2 !== 0"
-            >
-              Node
-            </v-list-item-title>
-            <v-autocomplete
-              v-model="nodeCategory"
-              :items="nodeCategories"
-              dense
-            />
-            <v-autocomplete
-              :items="nodeCategoryOptions"
-              dense
-            />
+          <v-list-item-content
+            v-if="i % 2 !== 0"
+          >
+            <v-row>
+              <v-list-item-title>
+                Node
+              </v-list-item-title>
+              <v-autocomplete
+                v-model="nodeCategory"
+                :items="nodeCategories"
+                dense
+              />
+              <v-autocomplete
+                :items="nodeCategoryOptions"
+                dense
+              />
+            </v-row>
           </v-list-item-content>
-          <v-list-item-content>
-            <v-list-item-title
-              v-if="i % 2 === 0"
-            >
-              Edge
-            </v-list-item-title>
-            <v-autocomplete
-              v-model="edgeCategory"
-              :items="edgeCategories"
-              dense
-            />
-            <v-autocomplete
-              :items="edgeCategoryOptions"
-              dense
-            />
+          <v-list-item-content
+            v-else
+          >
+            <v-row>
+              <v-list-item-title>
+                Edge
+              </v-list-item-title>
+              <v-autocomplete
+                v-model="edgeCategory"
+                :items="edgeCategories"
+                dense
+              />
+              <v-autocomplete
+                :items="edgeCategoryOptions"
+                dense
+              />
+            </v-row>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
@@ -59,54 +63,38 @@
 import Vue from 'vue';
 import store from '@/store';
 import { Node, Edge } from '@/types';
+import { computed, ref, Ref } from '@vue/composition-api';
 
-export default Vue.extend({
-  data(): {
-      hopsSelection: number[];
-      nodeQueryOptions: string[];
-      nodeCategory: string | null;
-      edgeQueryOptions: string[];
-      edgeCategory: string | null;
-      } {
+export default {
+  name: 'ConnectivityQuery',
+
+  setup() {
+    const hopsSelection = [1, 2, 3, 4, 5];
+    const nodeQueryOptions: Ref<string[]> = ref([]);
+    const edgeQueryOptions: Ref<string[]> = ref([]);
+    const nodeCategory: Ref<string> = ref('');
+    const edgeCategory: Ref<string> = ref('');
+    const selectedHops: Ref<number> = ref(1);
+    const displayedHops = computed(() => (selectedHops.value % 2 !== 0 ? selectedHops.value + 2 : selectedHops.value + 3));
+
+    const nodeCategories = computed(() => (store.state.network ? Object.keys(store.state.network.nodes[0]) : 'No attribute selected'));
+    const nodeCategoryOptions = computed(() => ((store.state.network && nodeCategory.value) ? store.state.network.nodes.map((n: Node) => n[nodeCategory.value]).sort() : 'No network'));
+
+    const edgeCategories = computed(() => (store.state.network ? Object.keys(store.state.network.edges[0]) : 'No attribute selected'));
+    const edgeCategoryOptions = computed(() => ((store.state.network && edgeCategory.value) ? store.state.network.edges.map((n: Edge) => n[edgeCategory.value]).sort() : 'No network'));
+
     return {
-      hopsSelection: [1, 2, 3, 4, 5],
-      nodeQueryOptions: [],
-      nodeCategory: '',
-      edgeQueryOptions: [],
-      edgeCategory: '',
+      hopsSelection,
+      nodeQueryOptions,
+      edgeQueryOptions,
+      nodeCategory,
+      edgeCategory,
+      displayedHops,
+      nodeCategories,
+      nodeCategoryOptions,
+      edgeCategories,
+      edgeCategoryOptions,
     };
   },
-  computed: {
-    selectedHops: {
-      get() {
-        return store.state.selectedHops;
-      },
-      set(value: number) {
-        store.commit.setSelectedHops(value);
-      },
-    },
-    displayedHops() {
-      if (store.state.selectedHops % 2 !== 0) { return store.state.selectedHops + 2; } return store.state.selectedHops + 3;
-    },
-    nodeCategories() {
-      if (store.state.network) { return Object.keys(store.state.network.nodes[0]); } return 'No network';
-    },
-    nodeCategoryOptions() {
-      if (store.state.network && this.nodeCategory) {
-        const attr: string = this.nodeCategory;
-        return store.state.network.nodes.map((n: Node) => n[attr]).sort();
-      } return 'No network';
-    },
-    edgeCategories() {
-      if (store.state.network) { return Object.keys(store.state.network.edges[0]); } return 'No network';
-    },
-    edgeCategoryOptions() {
-      if (store.state.network && this.edgeCategory) {
-        const edgeAttr: string = this.edgeCategory;
-        return store.state.network.edges.map((n: Edge) => n[edgeAttr]).sort();
-      } return 'No network';
-    },
-  },
-
-});
+};
 </script>
