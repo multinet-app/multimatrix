a75f51fa8e1af5a866c69c81eb26f183ebb0f00c
derya <deryakbaba22@gmail.com>
2021-06-22 22:22:45 -0600

    Change variable names

--- src/components/ConnectivityQuery.vue
+++ src/components/ConnectivityQuery.vue
@@ -26,8 +26,8 @@
             </v-col>
             <v-col class="pa-2">
               <v-autocomplete
-                v-model="nodeCategory[(i-1)/2]"
-                :items="nodeCategories[(i-1)/2]"
+                v-model="nodeVariable[(i-1)/2]"
+                :items="nodeVariableItems[(i-1)/2]"
                 dense
               />
             </v-col>
@@ -41,13 +41,13 @@
             <v-col class="pa-2">
               <v-autocomplete
                 v-if="nodeQuerySelection[(i-1)/2] === 'is (exact)'"
-                v-model="nodeCategorySelection[(i-1)/2]"
-                :items="nodeCategoryOptions[(i-1)/2]"
+                v-model="nodeVariableValue[(i-1)/2]"
+                :items="nodeVariableOptions[(i-1)/2]"
                 dense
               />
               <v-text-field
                 v-else
-                v-model="nodeCategorySelection[(i-1)/2]"
+                v-model="nodeVariableValue[(i-1)/2]"
                 dense
               />
             </v-col>
@@ -62,15 +62,15 @@
             </v-col>
             <v-col class="pa-2">
               <v-autocomplete
-                v-model="edgeCategory[i/2]"
-                :items="edgeCategories[i/2]"
+                v-model="edgeVariable[i/2]"
+                :items="edgeVariableItems[i/2]"
                 dense
               />
             </v-col>
             <v-col class="pa-2">
               <v-autocomplete
-                v-model="edgeCategorySelection[i/2]"
-                :items="edgeCategoryOptions[i/2]"
+                v-model="edgeVariableValue[i/2]"
+                :items="edgeVariableOptions[i/2]"
                 dense
               />
             </v-col>
@@ -109,38 +109,37 @@ export default {
     const selectedHops: Ref<number> = ref(1);
     const displayedHops = computed(() => 2 * selectedHops.value + 1);
     const nodeQueryOptions = ['is (exact)', 'contains'];
-    const edgeQueryOptions: Ref<string[]> = ref([]);
-    const nodeCategory: Ref<string[]> = ref([]);
-    const edgeCategory: Ref<string[]> = ref([]);
-    const nodeCategorySelection: Ref<string[]> = ref([]);
-    const edgeCategorySelection: Ref<string[]> = ref([]);
+    const nodeVariable: Ref<string[]> = ref([]);
+    const edgeVariable: Ref<string[]> = ref([]);
+    const nodeVariableValue: Ref<string[]> = ref([]);
+    const edgeVariableValue: Ref<string[]> = ref([]);
     const nodeQuerySelection: Ref<string[]> = ref([]);
-    const nodeCategories: Ref<string[][]> = ref([]);
-    const edgeCategories: Ref<string[][]> = ref([]);
-    const nodeCategoryOptions: string[][] = [];
-    const edgeCategoryOptions: string[][] = [];
+    const nodeVariableItems: Ref<string[][]> = ref([]);
+    const edgeVariableItems: Ref<string[][]> = ref([]);
+    const nodeVariableOptions: string[][] = [];
+    const edgeVariableOptions: string[][] = [];
 
     hopsSelection.forEach(() => {
-      nodeCategory.value.push(store.state.workspaceName === 'marclab' ? 'Label' : '');
-      edgeCategory.value.push(store.state.workspaceName === 'marclab' ? 'Type' : '');
-      nodeCategorySelection.value.push('');
-      edgeCategorySelection.value.push('');
+      nodeVariable.value.push(store.state.workspaceName === 'marclab' ? 'Label' : '');
+      edgeVariable.value.push(store.state.workspaceName === 'marclab' ? 'Type' : '');
+      nodeVariableValue.value.push('');
+      edgeVariableValue.value.push('');
       nodeQuerySelection.value.push('contains');
-      nodeCategories.value.push(store.state.network ? Object.keys(store.state.network.nodes[0]) : ['No network']);
-      edgeCategories.value.push(store.state.network ? Object.keys(store.state.network.edges[0]) : ['No network']);
+      nodeVariableItems.value.push(store.state.network ? Object.keys(store.state.network.nodes[0]) : ['No network']);
+      edgeVariableItems.value.push(store.state.network ? Object.keys(store.state.network.edges[0]) : ['No network']);
     });
 
     watchEffect(() => {
-      nodeCategory.value.forEach((category: string, i: number) => {
+      nodeVariable.value.forEach((category: string, i: number) => {
         // eslint-disable-next-line no-unused-expressions
-        store.state.network !== null ? nodeCategoryOptions[i] = store.state.network.nodes.map((n: Node) => n[category]).sort() : ['No attribute selected'];
+        store.state.network !== null ? nodeVariableOptions[i] = store.state.network.nodes.map((n: Node) => n[category]).sort() : ['No attribute selected'];
       });
     });
 
     watchEffect(() => {
-      edgeCategory.value.forEach((category: string, i: number) => {
+      edgeVariable.value.forEach((category: string, i: number) => {
         // eslint-disable-next-line no-unused-expressions
-        store.state.network !== null ? edgeCategoryOptions[i] = store.state.network?.edges.map((n: Edge) => n[category]).sort() : ['No attribute selected'];
+        store.state.network !== null ? edgeVariableOptions[i] = store.state.network?.edges.map((n: Edge) => n[category]).sort() : ['No attribute selected'];
       });
     });
 
@@ -149,13 +148,13 @@ export default {
       for (let i = 0; i < selectedHops.value + 1; i += 1) {
         const queryOperator = nodeQuerySelection.value[i] === 'is (exact)' ? '==' : '=~';
         if (i === 0) {
-          pathQueryText += `FILTER p.vertices[${i}].${nodeCategory.value[i]} ${queryOperator} '${nodeCategorySelection.value[i]}'`;
+          pathQueryText += `FILTER p.vertices[${i}].${nodeVariable.value[i]} ${queryOperator} '${nodeVariableValue.value[i]}'`;
         } else {
-          pathQueryText += ` AND p.vertices[${i}].${nodeCategory.value[i]} ${queryOperator} '${nodeCategorySelection.value[i]}'`;
+          pathQueryText += ` AND p.vertices[${i}].${nodeVariable.value[i]} ${queryOperator} '${nodeVariableValue.value[i]}'`;
         }
       }
       const queryOperator = nodeQuerySelection.value[0] === 'is (exact)' ? '==' : '=~';
-      const aqlQuery = `let startNodes = (FOR n in [${store.state.nodeTableNames}][**] FILTER n.${nodeCategory.value[0]} ${queryOperator} '${nodeCategorySelection.value[0]}' RETURN n) let paths = (FOR n IN startNodes FOR v, e, p IN 1..${selectedHops.value} ANY n GRAPH '${store.state.networkName}' ${pathQueryText} RETURN {nodes: p.vertices[*], paths: p}) let all_nodes = (for p in paths RETURN p.nodes) let nodes_first = (for p in paths RETURN p.nodes[0]) let nodes_last = (for p in paths RETURN p.nodes[${selectedHops.value}]) let path = (for p in paths RETURN p.paths) RETURN {all_nodes: UNIQUE(all_nodes[**]), nodes_first: UNIQUE(nodes_first), nodes_last: UNIQUE(nodes_last), paths: path}`;
+      const aqlQuery = `let startNodes = (FOR n in [${store.state.nodeTableNames}][**] FILTER n.${nodeVariable.value[0]} ${queryOperator} '${nodeVariableValue.value[0]}' RETURN n) let paths = (FOR n IN startNodes FOR v, e, p IN 1..${selectedHops.value} ANY n GRAPH '${store.state.networkName}' ${pathQueryText} RETURN {nodes: p.vertices[*], paths: p}) let all_nodes = (for p in paths RETURN p.nodes) let nodes_first = (for p in paths RETURN p.nodes[0]) let nodes_last = (for p in paths RETURN p.nodes[${selectedHops.value}]) let path = (for p in paths RETURN p.paths) RETURN {all_nodes: UNIQUE(all_nodes[**]), nodes_first: UNIQUE(nodes_first), nodes_last: UNIQUE(nodes_last), paths: path}`;
 
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       let newAQLNetwork: Promise<any[]> | undefined;
@@ -200,16 +199,15 @@ export default {
       selectedHops,
       displayedHops,
       nodeQueryOptions,
-      edgeQueryOptions,
-      nodeCategory,
-      edgeCategory,
-      nodeCategorySelection,
-      edgeCategorySelection,
+      nodeVariable,
+      edgeVariable,
+      nodeVariableValue,
+      edgeVariableValue,
       nodeQuerySelection,
-      nodeCategories,
-      nodeCategoryOptions,
-      edgeCategories,
-      edgeCategoryOptions,
+      nodeVariableItems,
+      nodeVariableOptions,
+      edgeVariableItems,
+      edgeVariableOptions,
       submitQuery,
     };
   },
