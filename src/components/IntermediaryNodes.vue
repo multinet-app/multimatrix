345656ffd5fd11c0033e7ea65ed6839d6088c35e
JackWilb <jackwilburn@tutanota.com>
2021-08-30 13:17:10 -0600

    Add cellSize to watcher

--- src/components/IntermediaryNodes.vue
+++ src/components/IntermediaryNodes.vue
@@ -219,7 +219,7 @@ export default defineComponent({
       buildIntView();
     });
 
-    watch(connectivityPaths, () => {
+    watch([connectivityPaths, cellSize], () => {
       processData();
       teardownOldView();
       buildIntView();
