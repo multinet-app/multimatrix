bf4ecf965c1d06c22532d13acce105943ca807f8
derya <gotdairyya@users.noreply.github.com>
2021-09-22 18:14:06 -0600

    Remove commented code
    Co-authored-by: Jack Wilburn <jackwilburn@tutanota.com>

--- src/components/PathTable.vue
+++ src/components/PathTable.vue
@@ -137,10 +137,6 @@ export default defineComponent({
           });
         }
       });
-      // // Removes extra edge in even paths
-      // if (pathLength.value % 2 === 0) {
-      //   toReturn.pop();
-      // }
       return toReturn;
     });
 
