7118124d3dc232692652976a05af9e315cca9839
Jack Wilburn <jackwilburn@tutanota.com>
2022-07-19 12:03:50 -0600

    Make sure we're not making 3-cycles

--- src/components/ConnectivityQuery.vue
+++ src/components/ConnectivityQuery.vue
@@ -401,9 +401,14 @@ export default defineComponent({
         } else if (!thisRoundIsNode) {
           currentString += `FOR n${nodeOrEdgeNum + 1}, e${nodeOrEdgeNum + 1} IN 1..1 ANY n${nodeOrEdgeNum} GRAPH '${store.state.networkName}' FILTER 1==1 `;
 
-          // If we have any node with nX where X is greater than 2, make sure we're not making cycles
+          // If we have any node with nX where X is greater than 2, make sure we're not making 2-cycles
           if (nodeOrEdgeNum + 1 > 1) {
             currentString += `AND n${nodeOrEdgeNum + 1} != n${nodeOrEdgeNum - 1} `;
+          }
+
+          // If we have any node with nX where X is greater than 3, make sure we're not making 3-cycles
+          if (nodeOrEdgeNum + 1 > 2) {
+            currentString += `AND n${nodeOrEdgeNum + 1} != n${nodeOrEdgeNum - 2} `;
           }
         }
 
